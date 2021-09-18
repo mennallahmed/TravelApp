2e7b7981b01af47775efa824e032fa7b850a575b
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2021-09-18 20:26:29 +0200

    Update countDown.js

--- src/client/js/countDown.js
+++ src/client/js/countDown.js
@@ -1,9 +1,11 @@
 let distance=0;
+let days=0;
 let x;
-function  TripDuration(inputText) {
+const TripDuration = async (inputText) => {
  
   const countDown = document.getElementById("travel-date").value;
   let countDownDate = new Date(countDown).getTime();
+  console.log(`entered Date:${countDownDate}`);
 
   //Clear the counter
   if (x) {
@@ -15,12 +17,12 @@ function  TripDuration(inputText) {
   
     // Get today's date and time
     let now = new Date().getTime();
-  
+    
     // Find the distance between now and the count down date
      distance = countDownDate - now;
   
     // Time calculations for days, hours, minutes and seconds
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
@@ -28,7 +30,10 @@ function  TripDuration(inputText) {
     // Display the result in the element with
     document.getElementById("irony").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
-  
+    console.log(`entered Date: ${countDownDate}`);
+    console.log(`now Date: ${now}`);
+    console.log(`distance: ${distance}`);
+    console.log(`days:${days} hours:${hours} minutes:${minutes} seconda:${seconds}`);
     // If the count down is finished, write some text
     if (distance < 0) {
       clearInterval(x);
@@ -37,4 +42,4 @@ function  TripDuration(inputText) {
   }, 1000);
 }
 
-export { TripDuration , distance}
+export { TripDuration , distance, days}
