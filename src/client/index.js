053cc12557fe8e51c6ac42cf587ed4ca5c6f6765
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2021-09-24 00:50:40 +0200

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,12 @@
-import { TripDuration ,distance, days } from './js/countDown'
+import { TripDuration } from './js/countDown'
 import { init } from './js/app'
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
+import img from './media/background.jpg';
+import img2 from './media/iconsprint.png'
 
 // Call init on DOMContentLoaded event.
 window.addEventListener('DOMContentLoaded', init);
@@ -12,7 +14,7 @@ window.addEventListener('DOMContentLoaded', init);
 
 export {
   TripDuration,
-  distance,
-  days,
-  init
+  init,
+  img,
+  img2
  }
