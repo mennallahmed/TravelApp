abcd9d10d7f4cdd8eb251b2f3ab3a821e6c76534
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2021-09-23 23:54:38 +0200

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const WorkboxPlugin = require('workbox-webpack-plugin');
+//const WorkboxPlugin = require('workbox-webpack-plugin');
 
 
 module.exports = {
@@ -46,6 +46,6 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
-        new WorkboxPlugin.GenerateSW()
+        //new WorkboxPlugin.GenerateSW()
     ]
 }
