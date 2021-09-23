ec846a4da44b467985fdbc236c40724ec433d56e
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2021-09-24 00:48:48 +0200

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,3 +1,5 @@
+import { img } from "..";
+
 async function init (){
 
   //Global Variables
@@ -12,6 +14,7 @@ async function init (){
   let restCountriesURL='';
   let newCity='';
 
+  //Get APIS data
   const getInfo = async (Geourl)=>{
     const res = await fetch(Geourl)
     try{
@@ -42,22 +45,46 @@ async function init (){
     }
   }
 
+  //Update the user interface with data
   const updateUI = async () => {
     const request = await fetch('http://localhost:8081/all');
     try {
       const allData = await request.json();
       console.log(allData);
-      document.getElementById('agreement').innerHTML = `Latitude: ${allData.latitude}`;
-      document.getElementById('subjectivity').innerHTML = `Longitude: ${allData.longitude}`;
-      document.getElementById('confidence').innerHTML = `Country: ${allData.country}`;
+      document.getElementById('city').innerHTML = `${newCity}`;
+      document.getElementById('country').innerHTML = `${allData.country}`;
+      document.getElementById('description').innerHTML = `${allData.description}`;
+      document.getElementById('temprature').innerHTML = `${allData.temp}℃`;
+      document.getElementById('currency').innerHTML = `${allData.currency}`;
+      document.getElementById('region').innerHTML = `- ${allData.region}`;
+      document.getElementById('language').innerHTML = `${allData.language}`; 
+      document.getElementById('icon').setAttribute("src", `https://www.weatherbit.io/static/img/icons/${allData.icon}.png`);
+      document.getElementById('image').setAttribute("src",`${allData.image}`);
+      document.getElementById("result").style.display = "flex";
+
     }catch(error){
       console.log('error', error);
     }
   }
+
+ //Event listener to print the trip info
+ document.getElementById("printbtn").addEventListener('click',printP);
+ async function printP(){
+  window.print();
+ } 
   
  document.getElementById('generate').addEventListener('click', handleSubmit);
  async function handleSubmit(event) {
   event.preventDefault()
+
+  //Calculate Trip Duration
+  const countDown = document.getElementById("travel-date").value;
+  let countDownDate = new Date(countDown).getTime();
+  // Get today's date and time
+  let now = new Date().getTime();      
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+  
 
   newCity = document.getElementById('name').value
   Geourl = `http://api.geonames.org/searchJSON?q=${newCity}&maxRows=1&username=${GeoNamesAPI_KEY}`; 
@@ -69,13 +96,13 @@ async function init (){
   const geoRes = await postData('http://localhost:8081/addGeo',{
     latitude: geoData.geonames[0].lat,
     longitude: geoData.geonames[0].lng,
-    country: geoData.geonames[0].countryName
+    country: geoData.geonames[0].countryName,
   });
 
-  if (Client.days< 7) {
+  if (distance< 604800000) {
   
    //Make call to Current Weather API If the trip is within a week
-   console.log(Client.days);
+   console.log(distance);
    weatherURL= `${currentWeatherURL}lat=${geoData.geonames[0].lat}&lon=${geoData.geonames[0].lng}&key=${weatherbitAPI_KEY}`;
    const weatherData =await getInfo(weatherURL);
    const weatherRes = await postData('http://localhost:8081/addweather',{
@@ -86,11 +113,10 @@ async function init (){
    console.log("Current Weather API") 
    console.log(weatherData);
    console.log(weatherRes);
-
   }
   else{
    //Make call to Weather Forecast API If the trip is within more than a week
-   console.log(Client.days);
+   console.log(distance);
    weatherURL=`${dailyForecastURL}lat=${geoData.geonames[0].lat}&lon=${geoData.geonames[0].lng}&key=${weatherbitAPI_KEY}`;
    const weatherData =await getInfo(weatherURL);
    const weatherRes = await postData('http://localhost:8081/addweather',{
@@ -101,9 +127,8 @@ async function init (){
    console.log("Forecast API")  
    console.log(weatherData);
    console.log(weatherRes);
-
   }
-
+  
   //Make call to Pixbay API 
   urlPixabay=`https://pixabay.com/api/?key=${PixabayAPI_KEY}&q=${newCity}&category=travel&image_type=photo`;
   const pixData =await getInfo(urlPixabay);
@@ -119,6 +144,7 @@ async function init (){
     region: restData[0].subregion,
     language: restData[0].languages[0].name
   });
+  await updateUI();
     
   
  }
